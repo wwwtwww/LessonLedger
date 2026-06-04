4287f830d2e60646eb50b3bf854344bc5782d227
wwwtwww <wwwtwww@gmail.com>
2026-06-04 10:14:54 +0800

    feat: add BottomSheetModalProvider to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,16 @@ import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LanguageProvider } from "../contexts/LanguageContext";
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <LanguageProvider>
-          <Stack screenOptions={{ headerShown: false }} />
+          <BottomSheetModalProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+          </BottomSheetModalProvider>
         </LanguageProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
