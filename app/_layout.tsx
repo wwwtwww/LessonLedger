219d87e0b6e4be665ad8c7a41937136aafa5aa75
wwwtwww <wwwtwww@gmail.com>
2026-06-04 09:41:05 +0800

    style: hide default expo router header and wrap with SafeAreaProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LanguageProvider } from "../contexts/LanguageContext";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <LanguageProvider>
-        <Stack />
-      </LanguageProvider>
+      <SafeAreaProvider>
+        <LanguageProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </LanguageProvider>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
